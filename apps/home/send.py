7b090e864f9ec246bbd4464cd9357a356b9e625b
Andrew Piispanen <apiispanen1@babson.edu>
2023-04-15 14:05:51 -0400

    Adding voices

--- apps/home/send.py
+++ apps/home/send.py
@@ -5,14 +5,16 @@ import base64
 
 def send_email(to_email='apiispanen@berkeley.edu', attachment=None, lyrics=None):
     try:
+        lyrics = lyrics.replace("\n", "<br>")
         from apps.home.creds import SENDGRID_KEY
     except:
         SENDGRID_KEY = os.getenv('SENDGRID_KEY')
+
     message = Mail(
-        from_email='appiispanen@gmail.com',
+        from_email='apiispanen@berkeley.edu',
         to_emails=[to_email, 'apiispanen@berkeley.edu'],
         subject='Your RingleDingle in this Thingle',
-        html_content=f'<strong>Ringles are great, especially with Dingles. Happy whatever day. Add some Ringle to your Dingle.</strong><br><br>Lyrics:{lyrics}')
+        html_content=f'<strong>Ringles are great, especially with Dingles. Happy whatever day. Add some Ringle to your Dingle.</strong><br><br><h4>Lyrics</h4><br><br>{lyrics}')
     
     if attachment:
         with open('apps/static/media/output.mp3', 'rb') as f:
